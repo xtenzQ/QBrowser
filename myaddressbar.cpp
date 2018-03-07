6e68e470374f94a12253a7a750295a2e0f6643ac
Nikita Rusetsky <rusetscky@outlook.com>
2018-03-07 18:48:27 +0800

    Deleting redundant files

--- myaddressbar.cpp
+++
@@ -1,8 +0,0 @@
-#include "myaddressbar.h"
-#include <QProgressBar>
-
-myAddressBar::myAddressBar(QWidget *parent) : UrlLineEdit(parent),
-     m_progressBar(new QProgressBar(this))
-{
-    m_progressBar->setSize(this->width(), this->height());
-}
